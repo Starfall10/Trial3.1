1b870bc9759b97518ab49c77179c72332f3228fb
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-10 21:43:33 +0100

    fixing line 4

--- Scripts/MovingObject.cs
+++ Scripts/MovingObject.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public abstract class MovingObject : MonoBehavious {
+public abstract class MovingObject : MonoBehaviours {
 
     public float moveTime = 0.1f;
     public LayerMask blockingLayer;
