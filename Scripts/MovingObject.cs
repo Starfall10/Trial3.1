50f6ec8a955457be05fb0332f36b2be0167f8f79
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-10 21:37:16 +0100

    added Move function

--- Scripts/MovingObject.cs
+++ Scripts/MovingObject.cs
@@ -18,6 +18,21 @@ public abstract class MovingObject : MonoBehavious {
         inverseMoveTime = 1f / moveTime;
     }
 
+    protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2 (xDir, yDir);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast (start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement (end));
+        }
+    }
+
     protected IEnumerator SmoothMovement (Vector3 end)
     {
         float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
