e3882a67d398b8780d917c0db055013f4df85ab7
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-14 16:56:03 +0100

    added variables, declear Awake and DamageWall function

--- Scripts/Wall.cs
+++ Scripts/Wall.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        spriteRenderer = getComponent<spriteRenderer>();
     }
 
+
+    public void DamageWall (int loss)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <= 0)
+            gameObject.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
