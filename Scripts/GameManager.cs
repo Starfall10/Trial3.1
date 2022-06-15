c4c83e94a530d424cc67263a79b6b7cd994a3c33
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-15 13:23:25 +0100

    declare function MoveEnemies

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -41,4 +41,23 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
 }
