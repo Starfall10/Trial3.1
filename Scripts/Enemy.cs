4c0c3f537d0339ccf5f0ee3f2777e880473d8ffd
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-15 12:10:15 +0100

    declare variables and declare protected function Start

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MovingObject
 {
-    // Start is called before the first frame update
-    void Start()
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform targer;
+    private bool skipMove;
+
+
+    protected override void Start()
     {
-        
+        animator = GetComponent<animator>();
+        target = GameObject.FindGameObjectWithTag ("Player").transform;
+        base.Start();
     }
 
     // Update is called once per frame
