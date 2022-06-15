97979c4d46ac7c80f4633e72e37edd9a2f79bdd9
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-15 15:48:24 +0100

    modified update function

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -35,6 +35,9 @@ public class Player : MovingObject
         int horizontal = 0;
         int vertical = 0;
 
+        horizontal = (int) Input.GetAxisRaw("Horizontal");
+        vertical = (int) Input.GetAxisRaw("Vertical");
+
         if (horizontal != 0)
             vertical = 0;
         
