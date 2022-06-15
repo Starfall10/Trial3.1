0070439b5f951c8177c5bc6cbbdf408beb1c0529
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-15 10:44:35 +0100

    declare protected function Start

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,14 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MovingObject
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+
+    protected override void Start () 
     {
-        
+        animator = GetComponet<animator>();
+
+        food - GameManager.instance.playerFoodPoints;
+
+        base.Start();
     }
 
+    
+    
     // Update is called once per frame
     void Update()
     {
