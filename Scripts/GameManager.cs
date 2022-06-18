96dce36056f240bd4bbb8b5821274f4c61bf8bb9
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-18 16:56:11 +0100

    modified function Update

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -63,7 +63,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playersTurn || enemiesMoving)
+        if (playersTurn || enemiesMoving || doingSetUp)
             return;
 
         StartCoroutine(MoveEnemies());
